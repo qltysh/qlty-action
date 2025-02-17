8ac32a1188d674c8bb706637ab3f2a00320fb592
Arslan <49312804+marschattha@users.noreply.github.com>
2025-02-18 03:07:04 +0500

    Log output when skipping errors (#44)
    * Add logs when skipping errors

--- coverage/src/main.ts
+++ coverage/src/main.ts
@@ -179,6 +179,8 @@ async function run(): Promise<void> {
   } catch {
     if (skipErrors) {
       core.warning('Error uploading coverage, skipping due to skip-errors')
+      core.warning('Output:')
+      core.warning(qlytOutput)
     } else {
       throw new CoverageUploadError(qlytOutput)
     }
