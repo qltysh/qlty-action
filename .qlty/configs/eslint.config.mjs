1ab75c8bc34abbd681a6a641881582417d920758
Bryan Helmkamp <19+brynary@users.noreply.github.com>
2025-04-14 09:49:29 -0400

    Remove prettier config (#57)

--- .qlty/configs/eslint.config.mjs
+++ .qlty/configs/eslint.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'eslint/config'
-import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs,ts}'],
+    files: ["**/*.{js,mjs,cjs,ts}"],
     plugins: { js },
-    extends: ['js/recommended']
+    extends: ["js/recommended"],
   },
-  tseslint.configs.recommended
-])
+  tseslint.configs.recommended,
+]);
